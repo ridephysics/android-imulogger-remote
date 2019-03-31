b423325ce2b697643535ad34cd1e68c3f5554dad
M1cha <sigmaepsilon92@gmail.com>
2019-03-31 14:57:11 +0200

    SampleGattAttributes: add current imulogger uuids

--- app/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ app/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -23,16 +23,13 @@ import java.util.HashMap;
  */
 public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
-    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
-    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+    public static String IMULOGGER_ACTIVE = "12345678-1234-5678-1234-56789abcdef1";
 
     static {
         // Sample Services.
-        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
-        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        attributes.put("12345678-1234-5678-1234-56789abcdef0", "IMU logger service");
         // Sample Characteristics.
-        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
-        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+        attributes.put(IMULOGGER_ACTIVE, "active");
     }
 
     public static String lookup(String uuid, String defaultName) {
