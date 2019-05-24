3ea00989cf24d5803239d0fcd0d9b434b6b65886
M1cha <sigmaepsilon92@gmail.com>
2019-05-24 23:23:06 +0200

    MainActivity: fix imports

--- app/src/main/java/com/ridephysics/imulogger_remote/MainActivity.kt
+++ app/src/main/java/com/ridephysics/imulogger_remote/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.ridephysics.imulogger_remote
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
